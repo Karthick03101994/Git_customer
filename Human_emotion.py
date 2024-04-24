d008f149007b660716d7ef03142edc9e05b4e60e
karthick <karthik03raj@gmail.com>
2024-04-24 17:48:36 +0530

    added human emotion

--- Human_emotion.py
+++ Human_emotion.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import streamlit as st 
-import joblib
+import pickle
 import spacy
+import joblib
 
 st.sidebar.header('Please chosse the menu')
 st.sidebar.selectbox(options=['Select the emotions example','sad','happy','angry'],label='selectbox')
@@ -10,8 +11,9 @@ st.sidebar.button('sumbit')
 
 new_text=st.text_input('Enter your text')
 
+with open('Human_emotion_new_project.pkl', 'rb') as f:
+    file = joblib.load(f)
 
-file=joblib.load("Human_emotion_new_project.pkl")
 Button=st.button('Enter')
 
 def pre_procsses(text):
@@ -20,7 +22,7 @@ def pre_procsses(text):
     proccessed_token= [token.lemma_ for token in doc if not token.is_stop and not token.is_punct and token.is_alpha]
     pre_procsses=' '.join(proccessed_token)
     return pre_procsses                  
-
+          
 if Button==True:
     new_text=pre_procsses(new_text)
     st.write(new_text)
