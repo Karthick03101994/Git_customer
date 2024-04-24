5f3a991a94968e2acd9385062ff503ed3a51d4c8
Karthick03101994 <155974783+Karthick03101994@users.noreply.github.com>
2024-04-24 17:55:39 +0530

    Update Human_emotion.py
    Made some changes

--- Human_emotion.py
+++ Human_emotion.py
@@ -1,15 +1,8 @@
 import pandas as pd
 import numpy as np
 import streamlit as st 
-<<<<<<< HEAD
-import pickle
 import spacy
 import joblib
-=======
-import spacy
-import pickle
-
->>>>>>> ed77321e257d5ec875dbf98cf1e508754c66af85
 
 st.sidebar.header('Please chosse the menu')
 st.sidebar.selectbox(options=['Select the emotions example','sad','happy','angry'],label='selectbox')
@@ -18,12 +11,8 @@ st.sidebar.button('sumbit')
 new_text=st.text_input('Enter your text')
 
 with open('Human_emotion_new_project.pkl', 'rb') as f:
-<<<<<<< HEAD
     file = joblib.load(f)
 
-=======
-    file = pickle.load(f)
->>>>>>> ed77321e257d5ec875dbf98cf1e508754c66af85
 Button=st.button('Enter')
 
 def pre_procsses(text):
